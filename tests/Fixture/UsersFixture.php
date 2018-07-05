fd54f79e5f84cd8328b367f55d7707772a6958ac
George Constantinou <georgec20001@gmail.com>
2018-07-05 10:49:33 +0300

    Added users fixture to support additional columns on users table (task #6493)

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Test\Fixture;
+
+use CakeDC\Users\Test\Fixture\UsersFixture as BaseFixture;
+
+/**
+ * UsersFixture
+ *
+ */
+class UsersFixture extends BaseFixture
+{
+    public $import = ['model' => 'Users'];
+}
