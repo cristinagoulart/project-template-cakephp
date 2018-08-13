7398d426e07003fb189c939bc84deb39137cb707
Andrey Vystavkin <jujav4ik@gmail.com>
2018-08-14 02:27:23 +0300

    Refactoring Scheduled Jobs and adding unit tests (task #6846)

---
+++ tests/TestCase/ScheduledJobs/Jobs/CakeShellJobTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Test\TestCase\ScheduledJobs\Jobs;
+
+use App\ScheduledJobs\Jobs\CakeShellJob;
+use PHPUnit\Framework\TestCase;
+
+class CakeShellJobText extends TestCase
+{
+
+    public function testRun()
+    {
+        $command = 'CakeShell::App:foobar';
+
+        $job = new CakeShellJob($command);
+
+        $this->assertEquals($command, $job->getCommand());
+        $this->assertEquals('', $job->getArguments());
+
+        $result = $job->run();
+
+        $this->assertEquals(244, $result['state']);
+        $this->assertNotEmpty($result['response']['stderr']);
+        $this->assertEmpty($result['response']['stdout']);
+        $this->assertContains('Exception: Shell class for "Foobar" could not be found.', $result['response']['stderr']);
+    }
+}
