ccc025078aacd72d7602512c31b8c2e1f1e45124
Michael Stepanov <michael@stepanoff.org>
2018-08-14 12:35:33 +0300

    Fixed logic to display email validation link

--- config/users.php
+++ config/users.php
@@ -3,10 +3,10 @@
 return [
     'Users.table' => 'Users',
     'Users.GoogleAuthenticator.login' => false,
-    'Users.Registration.active' => true,
+    'Users.Registration.active' => false,
     'Users.Tos.required' => false,
     'Users.Email.required' => true,
-    'Users.Email.validate' => true,
+    'Users.Email.validate' => false,
     // disable remember-me functionality because currently it affects google authenticator functionality:
     // https://github.com/CakeDC/users/issues/488
     'Users.RememberMe.active' => false,
