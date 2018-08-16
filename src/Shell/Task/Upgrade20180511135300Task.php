e9ba1f88af6be236752ab015e1f90071ead95309
George Constantinou <georgec20001@gmail.com>
2018-08-16 20:25:22 +0300

    Moved schedule job creation functionality into its own task (task #6718)

--- src/Shell/Task/Upgrade20180511135300Task.php
+++ src/Shell/Task/Upgrade20180511135300Task.php
@@ -1,32 +1,13 @@
 <?php
 namespace App\Shell\Task;
 
-use App\Feature\Factory as FeatureFactory;
-use App\ScheduledJobs\Handlers\CakeShellHandler;
 use Cake\Console\ConsoleOptionParser;
 use Cake\Console\Shell;
-use Cake\I18n\Time;
-use Cake\ORM\TableRegistry;
 
 class Upgrade20180511135300Task extends Shell
 {
     /**
-     * @var array $commandsToAdd for default deploy commands
-     */
-    public $commandsToAdd = [
-        'CakeShell::App:database_log' => [
-            // every 12 hours
-            'recurrence' => 'FREQ=HOURLY;INTERVAL=12',
-            'options' => 'cleanup',
-        ],
-        'CakeShell::CsvMigrations:import' => [
-            // every 5 minutes
-            'recurrence' => 'FREQ=MINUTELY;INTERVAL=5'
-        ]
-    ];
-
-    /**
-     * Manage the available sub-commands along with their arguments and help
+     * Configure option parser
      *
      * @return \Cake\Console\ConsoleOptionParser
      */
@@ -41,72 +22,21 @@ class Upgrade20180511135300Task extends Shell
     /**
      * main() method
      *
-     * @return null
+     * @return void
      */
     public function main()
     {
-        $feature = FeatureFactory::get('Module' . DS . 'ScheduledJobs');
-        if (! $feature->isActive()) {
-            $this->out('Scheduled Jobs are disabled. Skipping...');
+        $task = $this->Tasks->load('ScheduledJobs');
 
-            return false;
-        }
+        $task->add('CakeShell::App:database_log', [
+            // every 12 hours
+            'recurrence' => 'FREQ=HOURLY;INTERVAL=12',
+            'options' => 'cleanup',
+        ]);
 
-        $cakeShellsHandler = new CakeShellHandler();
-
-        $list = $cakeShellsHandler->getList();
-
-        foreach ($list as $command) {
-            if (in_array($command, array_keys($this->commandsToAdd))) {
-                $this->addScheduleJob($command);
-            }
-        }
-    }
-
-    /**
-     * Add Scheduled Job record if needed
-     *
-     * @param string $command to be added
-     * @return bool $result whether the record was added.
-     */
-    protected function addScheduleJob($command = '')
-    {
-        $result = false;
-        $scheduledJobsTable = TableRegistry::get('ScheduledJobs');
-
-        if (empty($command)) {
-            return $result;
-        }
-
-        $query = $scheduledJobsTable->find()
-            ->where(['job' => $command]);
-        $query->execute();
-
-        if ($query->count()) {
-            $entity = $query->first();
-            $this->warn("Scheduled Job [$command] already added. Status [$entity->active]");
-
-            return $result;
-        }
-
-        $entity = $scheduledJobsTable->newEntity();
-        $entity->name = "System [$command] command";
-        $entity->job = $command;
-        $entity->recurrence = $this->commandsToAdd[$command]['recurrence'];
-        $entity->active = true;
-        $entity->start_date = Time::now();
-
-        if (! empty($this->commandsToAdd[$command]['options'])) {
-            $entity->options = $this->commandsToAdd[$command]['options'];
-        }
-
-        if ($scheduledJobsTable->save($entity)) {
-            $this->success("Added Scheduled Job [$command] to the datatable.");
-            $result = true;
-        } else {
-            $this->warn("Error adding scheduled job [$command] to database");
-        }
-
-        return $result;
+        $task->add('CakeShell::CsvMigrations:import', [
+            // every 5 minutes
+            'recurrence' => 'FREQ=MINUTELY;INTERVAL=5'
+        ]);
     }
 }
