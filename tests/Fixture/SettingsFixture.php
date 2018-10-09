2b558cb7394c88fbe3f8a34dbaf8af92f86de567
Elia Mariutti <e.mariutti@qobo.biz>
2018-10-09 09:51:19 +0300

    test DbConfig (task #2048)

--- tests/Fixture/SettingsFixture.php
+++ tests/Fixture/SettingsFixture.php
@@ -9,29 +9,7 @@ use Cake\TestSuite\Fixture\TestFixture;
  */
 class SettingsFixture extends TestFixture
 {
-
-    /**
-     * Fields
-     *
-     * @var array
-     */
-    // @codingStandardsIgnoreStart
-    public $fields = [
-        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
-        'key' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
-        'value' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
-        '_indexes' => [
-            'key' => ['type' => 'index', 'columns' => ['key'], 'length' => []],
-        ],
-        '_constraints' => [
-            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
-        ],
-        '_options' => [
-            'engine' => 'InnoDB',
-            'collation' => 'latin1_swedish_ci'
-        ],
-    ];
-    // @codingStandardsIgnoreEnd
+    public $import = ['table' => 'settings'];
 
     /**
      * Init method
