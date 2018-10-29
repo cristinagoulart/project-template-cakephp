04f79ad4440f5eeebef47c76dd04acd077a07c0a
Elia Mariutti <e.mariutti@qobo.biz>
2018-10-29 17:09:36 +0200

    fix shell script and add in Robo (task #2048)

--- src/Shell/SettingsShell.php
+++ src/Shell/SettingsShell.php
@@ -51,11 +51,11 @@ class SettingsShell extends Shell
         $settings = $query->getAliasDiff(array_keys($alias));
 
         $data = [];
-        foreach ($alias as $set => $type) {
+        foreach ($settings as $set) {
             $data[] = [
                     'key' => $set,
                     'value' => Configure::read($set),
-                    'type' => $type // dynamic field to pass `type` to the validator
+                    'type' => $alias[$set] // dynamic field to pass `type` to the validator
                 ];
         }
 
