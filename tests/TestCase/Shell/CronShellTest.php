1e3e43d7de1b5d66f6ae22edda52d102d0232cd4
George Constantinou <georgec20001@gmail.com>
2018-11-07 17:19:23 +0200

    Tests for CronShell lock method (task #7968)

--- tests/TestCase/Shell/CronShellTest.php
+++ tests/TestCase/Shell/CronShellTest.php
@@ -65,4 +65,29 @@ class CronShellTest extends ConsoleIntegrationTestCase
         $this->exec('cron');
         $this->assertExitCode(Shell::CODE_SUCCESS);
     }
+
+    /**
+     * @dataProvider fileAndClassNamesProvider
+     * @return void
+     */
+    public function testLock($file, $class, $normalized) : void
+    {
+        $this->exec(sprintf('cron lock %s %s', $file, $class));
+
+        $expected = sprintf('%s%s_%s.lock.lock', sys_get_temp_dir() . DS, $normalized, md5($file));
+        $this->assertTrue(file_exists($expected));
+    }
+
+    public function fileAndClassNamesProvider() : array
+    {
+        return [
+            ['foo', 'bar', 'bar'],
+            ['foo1', 'bar1', 'bar1'],
+            ['foo_1234', 'bar__1', 'bar__1'],
+            ['foo\1234', 'b*a@r!5', 'b_a_r_5'],
+            ['foos', null, ''],
+            ['foos', '', ''],
+            ['foos', '1', '1']
+        ];
+    }
 }
