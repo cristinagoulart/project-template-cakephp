4f6a6fc946c3a3ea4f611d5d92db9eec212b97e5
Demetris Papademetriou <d.papademetrioug13@gmail.com>
2018-11-12 09:53:57 +0200

    Fixed some coding standards errors (task #5037)

--- config/routes.php
+++ config/routes.php
@@ -91,13 +91,14 @@ Router::scope('/', function (RouteBuilder $routes) {
  * URL Filter to redirect requests from DatabaseLog.DatabaseLogs to Logs
  */
 Router::addUrlFilter(function ($params, $request) {
-    if (empty($params['plugin']) || $params['plugin'] !== 'DatabaseLog' || empty($params['controller']) || $params['controller'] !== 'DatabaseLogs' ) {
+    if (empty($params['plugin']) || $params['plugin'] !== 'DatabaseLog' || empty($params['controller']) || $params['controller'] !== 'DatabaseLogs') {
         return $params;
     }
     if ($params['action'] === 'search') {
         $params['controller'] = 'Logs';
         $params['plugin'] = null;
     }
+
     return $params;
 });
 
