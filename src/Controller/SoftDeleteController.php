e70a8f3dbcc0d9afded9c60fd2b907ad375c003b
Elia Mariutti <e.mariutti@qobo.biz>
2018-11-20 11:08:14 +0200

    Contoller and working tests (task #2555)

--- src/Controller/SoftDeleteController.php
+++ src/Controller/SoftDeleteController.php
@@ -12,9 +12,7 @@ class SoftDeleteController extends AppController
     /**
      * Restore record
      *
-     * @param string $table table to search the id
-     * @param uuid $id id to delete
-     * @return bool Redirects to index.
+     * @return void
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
     public function restore()
@@ -30,25 +28,18 @@ class SoftDeleteController extends AppController
 
         if ($restore) {
             $this->Flash->success(__('The record is restored'));
-
-            return true;
         }
         $this->Flash->error(__('Can not restore the record. Please, try again.'));
-
-        return false;
     }
 
     /**
      * Delete permanently method
      *
-     * @param string $table table to search the id
-     * @param uuid $id id to delete
-     * @return bool Redirects to index.
+     * @return void
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
     public function delete()
     {
-        
         $this->request->allowMethod(['post']);
         $this->autoRender = false;
 
@@ -57,23 +48,18 @@ class SoftDeleteController extends AppController
 
         $toDel = $this->checkData($table, $id);
         $delete = TableRegistry::get($table)->removeBehavior('Trash')->delete($toDel);
-        
+
         if ($delete) {
             $this->Flash->success(__('The record is permanently delete'));
-
-            return true;
         }
         $this->Flash->error(__('Can not delete the record. Please, try again.'));
-
-        return false;
     }
 
     /**
      * Check data
      * @param string $table table to search the id
      * @param uuid $id id to delete
-     * @return \Cake\Datasource\EntityInterface|bool
-     * @throws \Exception
+     * @return \Cake\Datasource\EntityInterface|void
      */
     private function checkData($table, $id)
     {
