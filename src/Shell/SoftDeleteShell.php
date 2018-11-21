7833cbd2e6592e38d4f446860b8c5a1c407ab3fb
Elia Mariutti <e.mariutti@qobo.biz>
2018-11-21 16:55:08 +0200

    Improve shell script (task #2555)

--- src/Shell/SoftDeleteShell.php
+++ src/Shell/SoftDeleteShell.php
@@ -28,65 +28,137 @@ class SoftDeleteShell extends Shell
         $parser = parent::getOptionParser();
 
         $parser->setDescription('Permanently delete soft-deleted record');
-        $parser->addSubcommand('gc', [
-            'help' => 'Clean all records',
-            'parser' => [
-                'options' => [
-                    'age' => [
-                        'help' => __('Time to delete (ex: -1 day / -1 months)'),
-                        'required' => false,
-                        'short' => 'a'
-                    ]
-                ]
+        $parser->addArguments([
+            'module' => [
+                'help' => 'The module to clean from trashed records',
+                'required' => false,
+                'choices' => $this->validArgument()
             ],
+            'force' => [
+                'help' => 'Force the permanent delete',
+                'required' => false,
+                'choices' => ['force']
+            ]
+        ]);
+        $parser->addOptions([
+            'age' => [
+                'help' => __('Time to delete (ex: -1 day / -1 months)'),
+                'required' => false,
+                'short' => 'a'
+            ]
         ]);
 
         return $parser;
     }
 
     /**
-     * main() method.
+     * Main method.
+     * @return void|string
+     */
+    public function main()
+    {
+        $module = isset($this->args[0]) ? $this->args[0] : null;
+        // if no arguments will print the list of modules
+        if (!$module) {
+            $modules = $this->listModules();
+            foreach ($modules as $value) {
+                $this->out($value);
+            }
+
+            return;
+        }
+
+        if ($module === 'all') {
+            $this->cleanAll();
+
+            return;
+        }
+
+        $this->cleanModule($module);
+    }
+
+    /**
+     * Clean from trashed records all the Modules.
      *
      * @return null Success or error code.
      */
-    public function gc()
+    private function cleanAll()
     {
-        $age = $this->getDaysConfig();
-        // Delete before this day
-        $date = new Time($age);
-        // list all the Modules
-        $dir = new Folder(Configure::read('CsvMigrations.modules.path'));
-        $folders = $dir->read(true)[0];
+        $folders = $this->listModules();
+
         foreach ($folders as $module) {
-            $query = TableRegistry::get($module);
-
-            if (!$query->behaviors()->has('Trash')) {
-                // Module don't support trash
-                continue;
-            }
-
-            try {
-                $trashEntities = TableRegistry::get($module)->find('onlyTrashed');
-            } catch (\BadMethodCallException $e) {
-                // The module has not trash entities
-                continue;
-            }
-
-            $count = $query->removeBehavior('Trash')->deleteAll(['trashed <' => $date]);
+            $this->cleanModule($module);
         }
-
-        Log::write('info', "Clean up " . number_format($count) . " trash record older than $age.");
 
         return null;
     }
 
     /**
-     * Get stats log configuration
+     * If the argument 'force' is not set, it will display the trash info about the selected modules
+     * @param string $module Module to analyse/clean
+     * @return void
+     */
+    private function cleanModule($module)
+    {
+        $force = isset($this->args[1]);
+
+        $query = TableRegistry::get($module);
+
+        if (!$query->behaviors()->has('Trash')) {
+            // Module don't support trash
+            return;
+        }
+
+        try {
+            $trashEntities = TableRegistry::get($module)->find('onlyTrashed')->toArray();
+        } catch (\BadMethodCallException $e) {
+            // The module has not trash entities
+            return;
+        }
+
+        if (!$force) {
+            $this->out("The module $module had " . count($trashEntities) . " record(s) in the trash.");
+
+            return;
+        }
+
+        $age = $this->getDaysConfig();
+        $date = new Time($age);
+        $count = $query->removeBehavior('Trash')->deleteAll(['trashed <' => $date]);
+        Log::write('info', "Clean up $module: " . number_format($count) . " trash record older than $age.");
+    }
+
+    /**
+     * Get a list of all the CSV Modules
+     * @return array
+     */
+    private function listModules()
+    {
+        $dir = new Folder(Configure::read('CsvMigrations.modules.path'));
+        $folders = $dir->read(true)[0];
+
+        return (array)$folders;
+    }
+
+    /**
+     * Validate script arguments
+     * @return array list of CSV modules + 'all'
+     */
+    private function validArgument()
+    {
+        $args = $this->listModules();
+        $args[] = 'all';
+
+        return (array)$args;
+    }
+
+    /**
+     * Get age from configuration or parameter
      *
      * @return string
      * @throws \RuntimeException
      */
-    protected function getDaysConfig(): string
+    private function getDaysConfig(): string
     {
         if (isset($this->params['age'])) {
             return $this->params['age'];
