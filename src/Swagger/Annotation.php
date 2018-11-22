c8f3fb45106baee9cc3cd0c18d3b5d028e2e3ed8
Andrey Vystavkin <jujav4ik@gmail.com>
2018-11-22 16:10:06 +0200

    PHPStan errors resolved (task #7909)

--- src/Swagger/Annotation.php
+++ src/Swagger/Annotation.php
@@ -254,11 +254,11 @@ class Annotation
 
         $replacement = [$this->getInfo(), $this->getDefinition($this->getProperties()), $this->getPaths()];
         $replacement = implode("\n", $replacement) . "\n$1";
-        $content = file_get_contents($this->path);
+        $content = (string)file_get_contents($this->path);
 
         $pattern = '/(^class\s)/im';
         $content = preg_replace($pattern, $replacement, $content);
-        $content = trim($content);
+        $content = trim((string)$content);
 
         $this->setContent($content);
 
