5a3871ff3bf619a9132d0556c6534f4c5ce06893
George Constantinou <georgec20001@gmail.com>
2019-01-11 17:04:21 +0200

    Registering Search helper to the View (task #8668)

--- src/View/AppView.php
+++ src/View/AppView.php
@@ -13,6 +13,7 @@
  */
 namespace App\View;
 
+use Cake\ORM\TableRegistry;
 use Cake\View\View;
 
 /**
@@ -44,6 +45,10 @@ class AppView extends View
         $this->loadHelper('Menu.Menu');
         $this->loadHelper('Form', ['className' => 'AdminLTE.Form']);
         $this->loadHelper('HtmlEmail');
+        $this->loadHelper('Search', [
+            'table' => TableRegistry::get($this->request->getParam('controller')),
+            'id' => $this->request->getParam('pass.0')
+        ]);
         $this->loadHelper('SystemInfo');
         $this->loadHelper('CakeDC/Users.User');
     }
