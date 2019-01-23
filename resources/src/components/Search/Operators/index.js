cc400029196d69d01224d5bba36003f933aadc9f
George Constantinou <georgec20001@gmail.com>
2019-01-23 14:25:37 +0200

    Removed unused operator component (task #8668)

--- resources/src/components/Search/Operators/index.js
+++ resources/src/components/Search/Operators/index.js
@@ -1,19 +1,27 @@
+import blobOperator from './Text'
 import booleanOperator from './Boolean'
+import countryOperator from './Boolean'
+import currencyOperator from './Boolean'
 import datetimeOperator from './Number'
 import decimalOperator from './Number'
-import dblistOperator from './List'
+import dblistOperator from './Boolean'
 import emailOperator from './Text'
 import integerOperator from './Number'
-import listOperator from './List'
+import listOperator from './Boolean'
 import phoneOperator from './Text'
+import relatedOperator from './Boolean'
+import reminderOperator from './Number'
 import stringOperator from './Text'
-import sublistOperator from './List'
+import sublistOperator from './Boolean'
 import textOperator from './Text'
 import timeOperator from './Number'
 import urlOperator from './Text'
 
 export default {
+    blobOperator,
     booleanOperator,
+    countryOperator,
+    currencyOperator,
     datetimeOperator,
     decimalOperator,
     dblistOperator,
@@ -21,6 +29,8 @@ export default {
     integerOperator,
     listOperator,
     phoneOperator,
+    relatedOperator,
+    reminderOperator,
     stringOperator,
     sublistOperator,
     textOperator,
