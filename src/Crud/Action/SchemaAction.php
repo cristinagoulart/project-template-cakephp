40e52883ba371ade5c0d14fb099409e11d722acf
Elia Mariutti <e.mariutti@qobo.biz>
2019-02-25 12:35:04 +0200

    Add associations (task #10113)

--- src/Crud/Action/SchemaAction.php
+++ src/Crud/Action/SchemaAction.php
@@ -38,11 +38,29 @@ class SchemaAction extends BaseAction
     {
         $controller = $this->_controller()->getName();
         $mc = new ModuleConfig(ConfigType::MIGRATION(), $controller);
-        $data = $mc->parse();
+        $fields = (array)$mc->parse();
+        $db_fields_type = TableRegistry::getTableLocator()->get($controller)->getSchema()->typeMap();
+
+        $data_fields = [];
+        foreach ($fields as $field => $value) {
+            $value->{'db_type'} = preg_match('/^(money|metric)/', $value->type) ? 'integer': $db_fields_type[$value->name];
+            $data_fields[] = $value;
+        }
+
+        $data_association = [];
+        foreach ($this->_table()->associations() as $association) {
+            $data_association[] = [
+                'name' => $association->getName(),
+                'model' => $association->getTarget()->getTable(),
+                'type' => $association->type(),
+                'primary_key' => $association->getBindingKey(),
+                'foreign_key' => $association->getForeignKey()
+            ];
+        }
 
         $subject = $this->_subject(['success' => true]);
 
-        $this->_controller()->set('data', $data);
+        $this->_controller()->set('data', ['fields' => $data_fields, 'association' => $data_association]);
         $this->_trigger('beforeRender', $subject);
     }
 }
