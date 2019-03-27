4f8ad3307c848849593482a55808b48d3ea8e7ea
George Constantinou <georgec20001@gmail.com>
2019-03-27 15:18:22 +0200

    Added search export method (task #8668)

--- src/Controller/SearchTrait.php
+++ src/Controller/SearchTrait.php
@@ -1,7 +1,10 @@
 <?php
 namespace App\Controller;
 
+use Cake\Core\Configure;
 use Cake\Http\Exception\BadRequestException;
+use Cake\Utility\Hash;
+use Search\Utility\Export;
 
 trait SearchTrait
 {
@@ -20,5 +23,42 @@ trait SearchTrait
         }
 
         $this->render('/Module/search');
+    }
+
+    /**
+     * Export Search results
+     *
+     * Method responsible for exporting search results
+     * into a CSV file and forcing file download.
+     *
+     * @param string $id Saved search id
+     * @param string $filename Export filename
+     * @return \Cake\Http\Response|void
+     */
+    public function exportSearch(string $id, string $filename)
+    {
+        $filename = '' === trim($filename) ? $this->name : $filename;
+        $export = new Export($id, $filename, $this->Auth->user());
+
+        if ($this->request->is('ajax') && $this->request->accepts('application/json')) {
+            $page = (int)Hash::get($this->request->getQueryParams(), 'page', 1);
+            $limit = (int)Hash::get($this->request->getQueryParams(), 'limit', Configure::read('Search.export.limit'));
+
+            $export->execute($page, $limit);
+
+            $result = [
+                'success' => true,
+                'data' => ['path' => $export->getUrl()],
+                '_serialize' => ['success', 'data']
+            ];
+
+            $this->set($result);
+
+            return;
+        }
+
+        $this->set('count', $export->count());
+        $this->set('filename', $filename);
+        $this->render('Search.Search/export');
     }
 }
