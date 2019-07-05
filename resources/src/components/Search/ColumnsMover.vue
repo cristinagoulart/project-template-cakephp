875d46fc237230a2ac12e78a769986aa2191740d
Andrey Vystavkin <a.vystavkin@qobo.biz>
2019-07-05 12:41:27 +0300

    refactoring Search component (task #11872)

---
+++ resources/src/components/Search/ColumnsMover.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="col-xs-6 col-md-12">
+        <button type="button" @click="move('add')" class="btn btn-block btn-sm" :disabled="isGroupByEnabled || 0 === selectedColumns.available.length">
+            <span class="visible-md visible-lg"><i class="fa fa-angle-right"></i></span>
+            <span class="visible-xs visible-sm"><i class="fa fa-angle-down"></i></span>
+        </button>
+    </div>
+    <span class="visible-md visible-lg">&nbsp;</span>
+    <div class="col-xs-6 col-md-12">
+        <button type="button" @click="move('remove')" class="btn btn-block btn-sm" :disabled="isGroupByEnabled || 0 === selectedColumns.display.length">
+            <span class="visible-md visible-lg"><i class="fa fa-angle-left"></i></span>
+            <span class="visible-xs visible-sm"><i class="fa fa-angle-up"></i></span>
+        </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ColumnsMover',
+  props: ['selectedColumns', 'isGroupByEnabled'],
+  methods: {
+    move (action) {
+      this.$emit('column-moved', action)
+    }
+  }
+}
+</script>
---
+++ resources/src/components/Search/ColumnsSorter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="col-xs-6">
+        <button type="button" @click="sort('up')" :disabled="isOrderDisabled" class="btn btn-block btn-sm">
+            <i class="fa fa-angle-up"></i>
+        </button>
+    </div>
+    <div class="col-xs-6">
+        <button type="button" @click="sort('down')" :disabled="isOrderDisabled" class="btn btn-block btn-sm">
+            <i class="fa fa-angle-down"></i>
+        </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ColumnsSorter',
+  props: ['isOrderDisabled'],
+  methods: {
+    sort (direction) {
+      this.$emit('display-columns-sorted', direction)
+    }
+  }
+}
+</script>
