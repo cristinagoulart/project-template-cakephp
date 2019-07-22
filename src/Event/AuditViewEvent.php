5561a03796a00e1362a54ea7184bb5fc7d775732
Elia Mariutti <e.mariutti@qobo.biz>
2019-07-22 10:22:45 +0300

    Add Audit event (tast #11957)

---
+++ src/Event/AuditViewEvent.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Event;
+
+use AuditStash\Event\BaseEvent;
+
+class AuditViewEvent extends BaseEvent
+{
+    /**
+     * Returns the type name of this event object.
+     *
+     * @return string
+     */
+    public function getEventType()
+    {
+        return 'view';
+    }
+}
