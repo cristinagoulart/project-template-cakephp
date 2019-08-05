7de8e66709a1cfe6afc08b16cab96e42630f46eb
Elia Mariutti <e.mariutti@qobo.biz>
2019-08-05 16:49:42 +0300

    Set id field validation to uuid (task #11954)

--- src/Model/Table/ScheduledPersonalDataTable.php
+++ src/Model/Table/ScheduledPersonalDataTable.php
@@ -53,7 +53,7 @@ class ScheduledPersonalDataTable extends Table
     public function validationDefault(Validator $validator)
     {
         $validator
-            ->integer('id')
+            ->uuid('id')
             ->allowEmpty('id', 'create');
 
         $validator
