15e3537be5a818d3cfe1bdfafc2f7f53fd26abfb
George Constantinou <georgec20001@gmail.com>
2019-10-18 15:39:42 +0300

    Unit tests for MagicValue class (task #12103)

---
+++ tests/TestCase/Search/MagicValueTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\Test\TestCase\Search;
+
+use App\Search\MagicValue;
+use Cake\TestSuite\TestCase;
+
+/**
+ * Search\Utility\MagicValue Test Case
+ *
+ * @property array $user
+ */
+class MagicValueTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = ['id' => '00000000-0000-0000-0000-000000000002'];
+    }
+
+    public function tearDown()
+    {
+        unset($this->user);
+
+        parent::tearDown();
+    }
+
+    public function testConstructor(): void
+    {
+        $this->assertInstanceOf(MagicValue::class, new MagicValue('foo', $this->user));
+    }
+
+    public function testConstructorExceptionInvalidUserInfo(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        new MagicValue('foo', []);
+    }
+
+    public function testGetWithoutMagicValue(): void
+    {
+        $this->assertEquals('%%foo%%', (new MagicValue('%%foo%%', $this->user))->get());
+    }
+
+    public function testGetWithMagicValue(): void
+    {
+        $this->assertSame('00000000-0000-0000-0000-000000000002', (new MagicValue('%%me%%', $this->user))->get());
+        $this->assertEquals(
+            date('Y-m-d'),
+            (new MagicValue('%%today%%', $this->user))->get()
+        );
+        $this->assertEquals(
+            date('Y-m-d', time() - (24 * 60 * 60)),
+            (new MagicValue('%%yesterday%%', $this->user))->get()
+        );
+        $this->assertEquals(
+            date('Y-m-d', time() + (24 * 60 * 60)),
+            (new MagicValue('%%tomorrow%%', $this->user))->get()
+        );
+    }
+}
