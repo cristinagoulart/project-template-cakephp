26909b07ed7fbfad2a774aae381d8be19030e687
George Constantinou <georgec20001@gmail.com>
2019-10-21 10:17:21 +0300

    Fix assertion (task #12103)

--- tests/TestCase/Utility/ModelTest.php
+++ tests/TestCase/Utility/ModelTest.php
@@ -12,8 +12,8 @@ class ModelTest extends TestCase
     {
         $fields = Model::fields('Things');
 
-        // 30 migration fields, +4 from combined fields, +1 trashed field
-        $this->assertCount(35, $fields);
+        // 31 migration.json fields, +4 from combined fields, +1 trashed field
+        $this->assertCount(36, $fields);
 
         foreach ($fields as $field) {
             $this->assertArrayHasKey('name', $field);
