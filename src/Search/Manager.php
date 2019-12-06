53d6b82fca0489a5e89b26a84b9a5a361fe75344
George Constantinou <georgec20001@gmail.com>
2019-12-06 15:43:21 +0200

    Restore and deprecate public method to avoid BC break (task #12103)

--- src/Search/Manager.php
+++ src/Search/Manager.php
@@ -143,6 +143,90 @@ final class Manager
     }
 
     /**
+     * Method that formats search result-set.
+     *
+     * @param \Cake\Datasource\ResultSetInterface $entities Search result-set
+     * @param \Cake\ORM\Table $table Table instance
+     * @param bool $withPermissions Whether to include access permissions
+     * @return mixed[]
+     */
+    public static function formatEntities(ResultSetInterface $entities, Table $table, bool $withPermissions = false): array
+    {
+        deprecationWarning(
+            'Manager::formatEntities() method is deprecated. ' .
+            'Use \App\ORM\PrettyFormatter class instead.'
+        );
+
+        $result = [];
+        foreach ($entities as $entity) {
+            $result[] = self::formatEntity($entity, $table, $withPermissions);
+        }
+        return $result;
+    }
+
+    /**
+     * Method that formats search result-set entity.
+     *
+     * @param \Cake\Datasource\EntityInterface $entity Entity instance
+     * @param \Cake\ORM\Table $table Table instance
+     * @param bool $withPermissions Whether to include access permissions
+     * @return mixed[]
+     */
+    private static function formatEntity(EntityInterface $entity, Table $table, bool $withPermissions = false): array
+    {
+        static $factory = null;
+        if (null === $factory) {
+            $factory = new FieldHandlerFactory();
+        }
+        $result = [];
+        foreach (array_diff($entity->visibleProperties(), $entity->getVirtual()) as $field) {
+            // current table field
+            if ('_matchingData' !== $field) {
+                $result[$table->aliasField($field)] = $factory->renderValue($table, $field, $entity->get($field));
+                continue;
+            }
+            foreach ($entity->get('_matchingData') as $associationName => $relatedEntity) {
+                $result = array_merge($result, self::formatEntity(
+                    $relatedEntity,
+                    $table->getAssociation($associationName)->getTarget(),
+                    false
+                ));
+            }
+        }
+        if ($withPermissions) {
+            $primaryKey = $table->getPrimaryKey();
+            Assert::string($primaryKey);
+            $result['_permissions'] = self::getPermissions($entity->get($primaryKey), $table);
+        }
+        return $result;
+    }
+    /**
+     * Returns entity access permissions.
+     *
+     * @param string $id Entity ID
+     * @param \Cake\ORM\Table $table Table instance
+     * @return mixed[]
+     */
+    private static function getPermissions(string $id, Table $table): array
+    {
+        static $factory = null;
+        if (null === $factory) {
+            $factory = new AccessFactory();
+        }
+        list($plugin, $controller) = pluginSplit($table->getAlias());
+        $urls = [
+            'view' => ['prefix' => false, 'plugin' => $plugin, 'controller' => $controller, 'action' => 'view', $id],
+            'edit' => ['prefix' => false, 'plugin' => $plugin, 'controller' => $controller, 'action' => 'edit', $id],
+            'delete' => ['prefix' => false, 'plugin' => $plugin, 'controller' => $controller, 'action' => 'delete', $id]
+        ];
+        array_walk($urls, function (&$item, $key) use ($factory) {
+            $className = sprintf('\App\Controller\%sController', $item['controller']);
+            $item = $factory->hasAccess($item, User::getCurrentUser()) && method_exists($className, $item['action']);
+        });
+        return $urls;
+    }
+
+    /**
      * System search getter.
      *
      * @param string $model Model name
