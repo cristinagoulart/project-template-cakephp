3dccc281623c2a2233e70ed0f2c11f702c7c48e3
George Constantinou <georgec20001@gmail.com>
2020-01-24 15:41:28 +0200

    Extended LevelAwareDatabaseLog unit tests (task #15019)

--- tests/TestCase/Log/Engine/LevelAwareDatabaseLogTest.php
+++ tests/TestCase/Log/Engine/LevelAwareDatabaseLogTest.php
@@ -4,6 +4,7 @@ namespace App\Test\TestCase\Log\Engine;
 
 use App\Log\Engine\LevelAwareDatabaseLog;
 use Cake\Database\Log\LoggedQuery;
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
 use Psr\Log\LogLevel;
 
@@ -11,10 +12,21 @@ class LevelAwareDatabaseLogTest extends TestCase
 {
     public function testLog(): void
     {
+        $table = TableRegistry::getTableLocator()->get('DatabaseLogs');
+        $table->deleteAll([]);
+
         $engine = new LevelAwareDatabaseLog(['levels' => LogLevel::DEBUG]);
 
-        $this->assertEquals(false, $engine->log(LogLevel::DEBUG, new LoggedQuery()), "log() did not skip logging of database queries");
+        $this->assertFalse($engine->log(LogLevel::DEBUG, new LoggedQuery()), "log() did not skip logging of database queries");
+        $this->assertFalse($engine->log('bad_log_level', 'test message'), "log() did not skip logging of invalid log level");
 
-        $this->assertEquals(false, $engine->log('bad_log_level', 'test message'), "log() did not skip logging of invalid log level");
+        $message = 'test message';
+        $this->assertTrue($engine->log(LogLevel::DEBUG, $message));
+
+        $query = $table->find()->all();
+
+        $this->assertCount(1, $query);
+        $this->assertSame($message, $query->first()->get('message'));
+        $this->assertSame(LogLevel::DEBUG, $query->first()->get('type'));
     }
 }
