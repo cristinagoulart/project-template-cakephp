f58c2cb88cadda6cf5884614054c9a4a889ace7b
George Constantinou <georgec20001@gmail.com>
2020-01-24 12:22:01 +0200

    Add level configuration (task #15018)

--- tests/TestCase/Log/Engine/LevelAwareDatabaseLogTest.php
+++ tests/TestCase/Log/Engine/LevelAwareDatabaseLogTest.php
@@ -5,15 +5,15 @@ namespace App\Test\TestCase\Log\Engine;
 use App\Log\Engine\LevelAwareDatabaseLog;
 use Cake\Database\Log\LoggedQuery;
 use Cake\TestSuite\TestCase;
+use Psr\Log\LogLevel;
 
 class LevelAwareDatabaseLogTest extends TestCase
 {
     public function testLog(): void
     {
-        $engine = new LevelAwareDatabaseLog();
+        $engine = new LevelAwareDatabaseLog(['levels' => LogLevel::DEBUG]);
 
-        $loggedQuery = new LoggedQuery();
-        $this->assertEquals(false, $engine->log('debug', $loggedQuery), "log() did not skip logging of database queries");
+        $this->assertEquals(false, $engine->log(LogLevel::DEBUG, new LoggedQuery()), "log() did not skip logging of database queries");
 
         $this->assertEquals(false, $engine->log('bad_log_level', 'test message'), "log() did not skip logging of invalid log level");
     }
